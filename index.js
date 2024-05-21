fb426d1cf91d60fbf6cf06d7c44f570778858878
blackjac7 <febriansyah705@gmail.com>
2024-05-21 18:35:21 +0700

    initialize file

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello Binarians!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
