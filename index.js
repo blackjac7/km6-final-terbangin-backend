bbd59b2476848555c304f15c75959a84d25d558c
blackjac7 <febriansyah705@gmail.com>
2024-05-21 18:44:06 +0700

    initialize file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
