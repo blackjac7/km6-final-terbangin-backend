aa911cd826df23b0405629ae491f059c6ec56f0f
FauzanR06 <fauzanm157@gmail.com>
2024-06-07 17:44:29 +0700

    CRUD Airline

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const profileRoutes = require("./profile");
 const ticketRoutes = require("./ticket");
+const airlineRoutes = require("./airline");
 const authRoutes = require("./auth");
 const verificationRoutes = require("./verification");
 const { authMiddleware } = require("../middlewares/auth");
@@ -8,6 +9,8 @@ const { authMiddleware } = require("../middlewares/auth");
 router.use("/profile", authMiddleware, profileRoutes);
 
 router.use("/ticket", ticketRoutes);
+
+router.use("/airline", airlineRoutes);
 
 router.use("/auth", authRoutes);
 
