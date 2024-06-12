09683d8307c61a0f539dbf1febcebae8479476d6
Febriansyah <61476145+blackjac7@users.noreply.github.com>
2024-06-12 20:47:35 +0700

    fix-airline (#26)

--- usecases/airline/index.js
+++ usecases/airline/index.js
@@ -1,36 +1,27 @@
 const airlineRepo = require("../../repositories/airline");
-const { getAirlineBySerialNumber } = require("../../repositories/airline");
 
 exports.getAirlines = async () => {
-  const data = await airlineRepo.getAirlines();
-  return data;
+    const data = await airlineRepo.getAirlines();
+    return data;
 };
 
 exports.getAirline = async (id) => {
-  const data = await airlineRepo.getAirline(id);
-  return data;
+    const data = await airlineRepo.getAirline(id);
+    return data;
 };
 
 exports.createAirline = async (payload) => {
-  const existingSerialNumber = await getAirlineBySerialNumber(
-    payload.serialNumber
-  );
-
-  if (existingSerialNumber) {
-    throw { statusCode: 400, message: "serial number already used" };
-  }
-
-  const data = await airlineRepo.createAirline(payload);
-  return data;
+    const data = await airlineRepo.createAirline(payload);
+    return data;
 };
 
 exports.updateAirline = async (id, payload) => {
-  const data = await airlineRepo.updateAirline(id, payload);
-  await airlineRepo.getAirline(id);
-  return data;
+    const data = await airlineRepo.updateAirline(id, payload);
+    await airlineRepo.getAirline(id);
+    return data;
 };
 
 exports.deleteAirline = async (id) => {
-  const data = await airlineRepo.deleteAirline(id);
-  return data;
+    const data = await airlineRepo.deleteAirline(id);
+    return data;
 };
