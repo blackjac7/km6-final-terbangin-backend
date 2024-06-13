3529c655aad0e448f013ae7d26f66f4886aa7cb4
Resolt123 <126488778+Resolt123@users.noreply.github.com>
2024-06-13 08:45:48 +0700

    Flight v2 (#27)
    * membuat api get flight dapat memfilter data secara dinamis berdasarkan inputan user
    
    * menyelesaikan api flight untuk filter dan juga pengurangan capacity

--- repositories/flight/index.js
+++ repositories/flight/index.js
@@ -6,11 +6,81 @@ exports.getFlights = async () => {
       {
         model: Airlines,
       },
-      { model: Airports, as: "StartAirport" },
-      { model: Airports, as: "EndAirport" },
+      {
+        model: Airports,
+        as: "StartAirport",
+      },
+      {
+        model: Airports,
+        as: "EndAirport",
+      },
     ],
   });
+
   return data;
+};
+
+exports.getFlightsbyFilter = async (key, value, filter, order, start, end) => {
+  if (key && value) {
+    const data = await Flights.findAll({
+      order: [[filter, order]],
+      where: {
+        [key]: value,
+      },
+      include: [
+        {
+          model: Airlines,
+        },
+        {
+          model: Airports,
+          as: "StartAirport",
+          where: {
+            city: start,
+          },
+        },
+        {
+          model: Airports,
+          as: "EndAirport",
+          where: {
+            city: end,
+          },
+        },
+      ],
+    });
+
+    if (data.length) {
+      return data;
+    }
+    return "data tidak ditemukan";
+  } else {
+    const data = await Flights.findAll({
+      order: [[filter, order]],
+      include: [
+        {
+          model: Airlines,
+        },
+        {
+          model: Airports,
+          as: "StartAirport",
+          where: {
+            city: start,
+          },
+        },
+        {
+          model: Airports,
+          as: "EndAirport",
+          where: {
+            city: end,
+          },
+        },
+      ],
+    });
+
+    if (data.length) {
+      return data;
+    }
+    return "data tidak ditemukan";
+  }
 };
 
 exports.getFlightbyId = async (id) => {
@@ -36,6 +106,31 @@ exports.getFlightbyId = async (id) => {
 exports.createFlight = async (payload) => {
   const data = await Flights.create(payload);
   return data;
+};
+
+exports.decrementFlightCapacity = async (seatclass, value, id) => {
+  const flight = await Flights.findOne({
+    where: { id: id },
+    attributes: ["capacity" + seatclass],
+  });
+
+  if (!flight) {
+    throw new Error(`Flight dengan ID ${id} tidak ditemukan.`);
+  }
+
+  const currentCapacity = flight["capacity" + seatclass];
+  if (currentCapacity < value) {
+    throw new Error(
+      `Tidak bisa mengurangi kapasitas sebanyak ${value} karena hanya ada ${currentCapacity} kursi yang tersedia.`
+    );
+  }
+
+  await Flights.increment(
+    { ["capacity" + seatclass]: -value },
+    { where: { id: id } }
+  );
+
+  return `capacity telah berhasil di kurangi sebanyak ${value}`;
 };
 
 exports.updateFlight = async (id, payload) => {
--- usecases/flight/index.js
+++ usecases/flight/index.js
@@ -5,6 +5,18 @@ exports.getFlights = async () => {
   return data;
 };
 
+exports.getFlightsbyFilter = async (key, value, filter, order, start, end) => {
+  const data = await flightsRepo.getFlightsbyFilter(
+    key,
+    value,
+    filter,
+    order,
+    start,
+    end
+  );
+  return data;
+};
+
 exports.getFlightbyId = async (id) => {
   const data = await flightsRepo.getFlightbyId(id);
   return data;
@@ -14,6 +26,11 @@ exports.createFlight = async (payload) => {
   const data = await flightsRepo.createFlight(payload);
   return data;
 };
+
+exports.decrementFlightCapacity = async (seatclass, value, id) => {
+  const data = await flightsRepo.decrementFlightCapacity(seatclass,value,id);
+  return data;
+}
 
 exports.updateFlight = async (id, payload) => {
   const data = await flightsRepo.updateFlight(id, payload);
