45ab1c5aeb1a6e71dc2f52bb207bb1f5858e028f
blackjac7 <febriansyah705@gmail.com>
2024-06-28 14:52:44 +0700

    modify datatype payment decimal to 15, 2

--- models/payments.js
+++ models/payments.js
@@ -15,49 +15,49 @@ module.exports = (sequelize, DataTypes) => {
                 onUpdate: "CASCADE",
             });
             Payments.hasOne(models.Bookings, {
-              foreignKey: "paymentId",
-              onDelete: "CASCADE",
-              onUpdate: "CASCADE",
+                foreignKey: "paymentId",
+                onDelete: "CASCADE",
+                onUpdate: "CASCADE",
             });
         }
     }
     Payments.init(
-      {
-        id: {
-          allowNull: false,
-          primaryKey: true,
-          type: DataTypes.UUID,
+        {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: DataTypes.UUID,
+            },
+            userId: {
+                allowNull: false,
+                type: DataTypes.UUID,
+            },
+            status: {
+                defaultValue: "UNPAID",
+                type: DataTypes.ENUM("ISSUED", "UNPAID", "CANCELLED"),
+            },
+            method: {
+                type: DataTypes.STRING,
+            },
+            snapLink: {
+                type: DataTypes.TEXT,
+            },
+            snapToken: {
+                type: DataTypes.TEXT,
+            },
+            totalPrice: {
+                type: DataTypes.DECIMAL(15, 2),
+                allowNull: false,
+            },
+            deletedAt: {
+                type: DataTypes.DATE,
+            },
         },
-        userId: {
-          allowNull: false,
-          type: DataTypes.UUID,
-        },
-        status: {
-          defaultValue: "UNPAID",
-          type: DataTypes.ENUM("ISSUED", "UNPAID", "CANCELLED"),
-        },
-        method: {
-          type: DataTypes.STRING,
-        },
-        snapLink: {
-          type: DataTypes.TEXT,
-        },
-        snapToken: {
-          type: DataTypes.TEXT,
-        },
-        totalPrice: {
-          type: DataTypes.DECIMAL(10, 2),
-          allowNull: false,
-        },
-        deletedAt: {
-          type: DataTypes.DATE,
-        },
-      },
-      {
-        sequelize,
-        modelName: "Payments",
-        paranoid: true,
-      }
+        {
+            sequelize,
+            modelName: "Payments",
+            paranoid: true,
+        }
     );
     return Payments;
 };
