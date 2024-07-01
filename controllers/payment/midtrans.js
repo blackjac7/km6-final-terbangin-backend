9aa63b278137e73e5cdf45aad7c9be4c3f0db869
Febriansyah <61476145+blackjac7@users.noreply.github.com>
2024-07-01 19:10:14 +0700

    Fix bug 3 (#56)
    * updates seat when payment success
    
    * update socket, template cetak tiket, add package puppeter
    
    * add socket
    
    * socket seat
    
    * template done
    
    * fix bug
    
    * fix bug
    
    * fix

--- controllers/payment/midtrans.js
+++ controllers/payment/midtrans.js
@@ -4,30 +4,14 @@ exports.handleMidtransNotification = async (req, res, next) => {
     try {
         const payload = req?.body;
 
-        const data = await midtransUsecase.handleMidtransNotification(payload);
+        const data = await midtransUsecase.handleMidtransNotification(
+            payload,
+            req
+        );
 
-        // if (data.status === "ISSUED") {
-        //     req.io.emit("paymentSuccess", {
-        //         message: `Pembayaran berhasil dibayar sebesar`,
-        //         highlight: `Rp ${data.totalPrice}`,
-        //     });
-
-        //     req.io.emit("seatsUpdate", {
-        //         message: "Seats Update",
-        //     });
-        // } else if (data.status === "CANCELLED") {
-        //     req.io.emit("paymentFailed", {
-        //         message: `Pembayaran gagal untuk transaksi`,
-        //         highlight: `Rp ${data.totalPrice}`,
-        //     });
-        //     req.io.emit("seatsUpdate", {
-        //         message: "Seats Update",
-        //     });
-        // }
-
-        // req.io.emit("notificationUpdate", {
-        //     message: "Notification Update",
-        // });
+        req.io.emit("notificationUpdate", {
+            message: "Notification Update",
+        });
 
         return res.status(200).json({
             data,
